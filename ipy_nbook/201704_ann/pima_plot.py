8009c8c450e6332a4f9d35a572bb5ac503c2a41b
neoinubuntu <jd-linjian@163.com>
2017-04-30 19:21:37 +0800

    20170430-1493551297

---
+++ ipy_nbook/201704_ann/pima_plot.py
@@ -0,0 +1,32 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+
+train=pd.read_csv("./pima.csv",header=None)
+train_normal=(train-train.mean())/(train.max()-train.min())
+train_normal[8]=train[8]
+
+features =train_normal[:][[i for i in range(8)]].values
+target = train_normal[:][8]
+labels=["nopima","pima"]
+feature_names=[i for i in range(9)]
+colors=['r','y','g','b']
+pairs=[(i,8) for i in range(8)]
+for i,(p0,p1) in enumerate(pairs):
+    # print i,(p0,p1)
+    plt.subplot(4,2,i+1)
+    marker="."
+    c=colors[i%4]
+    plt.scatter(features[:,p0], target, marker=marker, c=c)
+    plt.xlabel(feature_names[p0])
+    plt.ylabel(p0)
+# plt.show()
+
+ref_matrix=np.zeros([9,9])
+for i in range(9):
+    for j in range(9):
+        ref_matrix[i][j]=train[i].corr(train[j])
+# print ref_matrix
+from utils import *
+plot_confusion_matrix(ref_matrix,[i for i in range(10)],'Correlation','Correlation')
+
---
+++ ipy_nbook/201704_ann/utils.py
@@ -0,0 +1,46 @@
+import os
+
+from matplotlib import pylab
+import numpy as np
+
+def plot_confusion_matrix(cm, genre_list, name, title,max,save=False):
+    pylab.clf()
+    pylab.matshow(cm, fignum=False, cmap='Greens', vmin=0, vmax=max)
+    ax = pylab.axes()
+    ax.set_xticks(range(len(genre_list)))
+    ax.set_xticklabels(genre_list)
+    ax.xaxis.set_ticks_position("bottom")
+    ax.set_yticks(range(len(genre_list)))
+    ax.set_yticklabels(genre_list)
+    pylab.title(title)
+    pylab.colorbar()
+    # pylab.grid(True)
+    pylab.show()
+    pylab.xlabel('Predicted class')
+    pylab.ylabel('True class')
+    pylab.grid(True)
+    if save==True:
+        pylab.savefig(os.path.join(CHART_DIR, "confusion_matrix_%s.png"%name), bbox_inches="tight")
+
+
+
+def plot_confusion_matrix2(cm, genre_list, name, title,save=False):
+    pylab.clf()
+    pylab.matshow(cm, fignum=False, cmap='Greens', vmin=0, vmax=1.0)
+    ax = pylab.axes()
+    ax.set_xticks(np.arange(0,len(genre_list),0.9))
+    ax.set_xticklabels(genre_list)
+    ax.xaxis.set_ticks_position("bottom")
+    ax.set_yticks(np.arange(0,len(genre_list),0.9))
+    ax.set_yticklabels(genre_list)
+    pylab.title(title)
+    pylab.colorbar()
+    # pylab.grid(True)
+    pylab.show()
+    pylab.xlabel('x class')
+    pylab.ylabel('y class')
+    pylab.grid(True)
+    if save==True:
+        pylab.savefig(os.path.join(CHART_DIR, "confusion_matrix_%s.png"%name), bbox_inches="tight")
+
+
