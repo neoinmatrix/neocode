5361be14ca64b5973b201994c9cbc93551d9c092
neoinubuntu <jd-linjian@163.com>
2017-05-15 22:55:53 +0800

    20170515-1494860153

--- python/20170515/pca_bak.py
+++ python/20170515/pca_bak.py
@@ -1,41 +1,51 @@
 import numpy as np 
 
-def zeroMean(X):        
-    means=np.mean(X,axis=1)
-    X_mean=X
-    for i in range(len(X_mean)):
-        X_mean[i]=X_mean[i]-means[i]
-    return X_mean,means
+def percentage_n(lbd,percentage):
+    sort=np.sort(lbd) 
+    sort=sort[-1::-1] 
+    sumall=sum(sort)*percentage
+    for i in range(1,len(lbd)+1):
+        if sum(sort[:i])>=sumall:
+            break
+    return i 
 
-def percentage_n(lambdaVal,percentage):
-    sortArray=np.sort(lambdaVal) 
-    sortArray=sortArray[-1::-1] 
-    arraySum=sum(sortArray)
-    tmpSum=0
-    num=0
-    for i in sortArray:
-        tmpSum+=i
-        num+=1
-        if tmpSum>=arraySum*percentage:
-            return num
+def pca(A,percentage=0.99):
+    dims,nums=A.shape
+    means=np.mean(A,axis=1)
+    Amean=A
+    for i in range(dims):
+        Amean[i]=Amean[i]-means[i]
+    covA=np.dot(A,A.T)/float(nums)
+    lbd,U=np.linalg.eig(np.mat(covA))
+    k=percentage_n(lbd,percentage)
+    index_lbd=np.argsort(lbd)
+    index_lbd=index_lbd[-1::-1]
+    k_index=index_lbd[:k]
+    k_U=U[:,k_index] 
 
-def pca(X_raw,percentage=0.99):  
-    X,means=zeroMean(X_raw) 
-    cov=np.dot(X,X.T)/len(X[0])
-    lambdaVal,P=np.linalg.eig(np.mat(cov))
-    k=percentage_n(lambdaVal,percentage)
-    sortlambadaVal=np.argsort(lambdaVal)   
-    k_lambadaIndex=sortlambadaVal[-1:-(k+1):-1]  
-    k_P=P[:,k_lambadaIndex] 
-
-    Y=k_P.T*X_raw  
-    print  k_P.T*k_P
-    newX=k_P*k_P.T*X_raw 
+    Y=k_U.T*A  
+    # print Y.shape
+    # print  k_P.T*k_P
+    newX=k_U.T*A*k_U 
     for i in range(len(newX)):
         newX[i]+=means[i]
     print newX
     exit()
     return Y,newX 
-data=np.array([[-1,-1,0,2,0],[-2,0,0,1,1]],dtype=np.float)
+A=np.array([[-1,-1,0,2,0],[-2,0,0,1,1]],dtype=np.float)
+
 # print data
-Y,newX=pca(data,0.8)
+Y,newX=pca(A,0.8)
+# print len([])
+# a=A[0]
+# print a
+# b=np.argsort(a)
+# print b
+# print b[-1::-1]
+
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.scatter(dataMat[:,0].flatten().A[0],dataMat[:,1].flatten().A[0],marker='^',s=90)
+# ax.scatter(reconMat[:,0].flatten().A[0],reconMat[:,1].flatten().A[0],marker='o',s=50,c='red')
+# plt.show()
