8c70862852fefaaa8bc9bcce1f06005ee69134d3
neo <jd-linjian@163.com>
2017-06-01 21:17:50 +0800

    '20170601'

---
+++ python/20170531mr/draw3d.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt  
+from mpl_toolkits.mplot3d import Axes3D  
+fig = plt.figure()  
+ax = fig.add_subplot(111, projection='3d')  
+X = [1, 1, 2, 2]  
+Y = [3, 4, 4, 3]  
+Z = [1, 2, 1, 1]  
+ax.scatter(X, Y, Z)  
+plt.show()  
