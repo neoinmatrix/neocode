b03e5c6596bfb8495bf2ee0ccf302393bc9c7722
neo <jd-linjian@163.com>
2017-06-07 08:38:16 +0800

    '20170607'

--- ml/20170531mr/tryall.py
+++ ml/20170531mr/tryall.py
@@ -50,12 +50,12 @@ def getvector(idx,mouse,goal,label):
             if state*y[i]<0:
                 ychange+=1
             state=y[i]
-        return [ychange]
+        return [float(ychange)]
     def gett(mouse):
         tn=len(mouse[2])
         t=mouse[2]
         if tn==1:
-            return [0]
+            return [0.0]
         return [(t[tn-1]-t[0])/1000]
     def getxspeed(mouse):
         tn=len(mouse[2])
@@ -71,13 +71,13 @@ def getvector(idx,mouse,goal,label):
             else:
                 x[i]=0.0
         x=np.array(x)[1:]
-        return [x.mean()]
+        return [x.mean()/10]
 
     tmp=[]
-    tmp.extend([mouse[0][0],mouse[1][0]/2000])
+    tmp.extend([mouse[0][0]/1000,mouse[1][0]/1000])
     # tmp.extend(goalend(mouse,goal))
     # tmp.extend(startend(mouse))
-    # tmp.extend(getyn(mouse))
+    tmp.extend(getyn(mouse))
     # tmp.extend(gett(mouse))
     # tmp.extend(getxspeed(mouse))
     # np.array([xarr[0],yarr[0]]).reshape([1,2])
@@ -105,9 +105,10 @@ def assemble():
 
 def getAssembleResult():
     ds=dataset.DataSet()
+    # ds.test_file_path=ds.train_file_path
     ds.getTrainData()
     dt=datadeal.DataTrain()
-    clf=SVC()
+    clf=SVC(C=0.55)
     # clf = MLPClassifier(alpha=1e-5, hidden_layer_sizes=(10), random_state=1)
     y=ds.train["labels"]
     mouses=ds.train["mouses"]
@@ -117,13 +118,115 @@ def getAssembleResult():
     X=[]
     for i in range(n):
         X.append(getvector(1,mouses[i],goals[i],1)[0])
+        # print X
+        # exit()
     X=np.array(X)
     # print X.shape
     # dt.trainTest(clf,X,y)
     dt.train(clf,X,y)
+
+    # dt.testResultAll(ds,getvector,savepath='./data/0607tmp.txt')
+
     dt.testResultAll(ds,getvector,savepath='./data/assemble.txt')
+
+def dataanalyst():
+    with open('./data/0607tmp.txt','r') as f:
+        r=f.read()
+    ra=r.split('\n')
+    # print ra
+    wrong=[]
+    notget=[]
+    tmparr=[0]*3000
+    strwrong=''
+    strnotget=''
+    for v in ra:
+        if v=='':
+            continue
+        if int(v)<2600:
+            wrong.append(v)
+            strwrong+=v+"\n"
+        tmparr[int(v)-1]=1
+    for i in range(2600,3000):
+        if tmparr[i]==0:
+            notget.append(i+1)
+            strnotget+=str(i+1)+"\n"
+    # print wrong,len(wrong)
+    # print notget,len(notget)
+
+    with open('./data/wrong.txt','w') as f:
+        f.write(strwrong)
+    with open('./data/notget.txt','w') as f:
+        f.write(strnotget)
+
+def wronganalyst():
+    ds=dataset.DataSet()
+    ds.getTrainData()
+    dt=datadeal.DataTrain()
+    
+    y=ds.train["labels"]
+    mouses=ds.train["mouses"]
+    goals=ds.train["goals"]
+    labels=ds.train["labels"]
+
+    with open('./data/wrong.txt','r') as f:
+        s=f.read()
+    wrongs=s.split()
+    idxw=np.array(wrongs,dtype='int')-1
+    # print len(idxw)
+    mousew=[mouses[i] for i in idxw]
+    # print len(mousew)
+    # mousew=mouses[]
+    # print idxw
+    dw=datadraw.DataDraw('3d')
+    colors=dw.getColorsValue()
+    count=0
+    dw.draw3dline(mouses[2601],c=colors[count%10])
+    for i in range(len(mousew)):
+        count+=1
+        if count>4:
+            break
+        dw.draw3dline(mousew[i],c=colors[count%10])
+        # goal=goals[i]
+        # dw.draw3dgoal([goal[0],goal[1]+2000,0],c=colors[count%10])
+    plt.show()
+
+def notgetanalyst():
+    ds=dataset.DataSet()
+    ds.getTrainData()
+    dt=datadeal.DataTrain()
+    
+    y=ds.train["labels"]
+    mouses=ds.train["mouses"]
+    goals=ds.train["goals"]
+    labels=ds.train["labels"]
+
+    with open('./data/notget.txt','r') as f:
+        s=f.read()
+    wrongs=s.split()
+    idxw=np.array(wrongs,dtype='int')-1
+    # print len(idxw)
+    mousew=[mouses[i] for i in idxw]
+    # print len(mousew)
+    # mousew=mouses[]
+    # print idxw
+    dw=datadraw.DataDraw('3d')
+    colors=dw.getColorsValue()
+    count=0
+    dw.draw3dline(mouses[0],c=colors[count%10])
+    for i in range(len(mousew)):
+        count+=1
+        if count>10:
+            break
+        dw.draw3dline(mousew[i],c=colors[count%10])
+        # goal=goals[i]
+        # dw.draw3dgoal([goal[0],goal[1]+2000,0],c=colors[count%10])
+    plt.show()
+
 
 if __name__=="__main__":
     # assemble()
     getAssembleResult()
+    # dataanalyst()
+    # wronganalyst()
+    # notgetanalyst()
     pass
