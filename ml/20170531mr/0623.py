71b4000863c790e772a8bca78b0ea10e4d4438a3
neo <jd-linjian@163.com>
2017-06-24 15:34:10 +0800

    20170624-1498289649

--- ml/20170531mr/0623.py
+++ ml/20170531mr/0623.py
@@ -10,37 +10,12 @@ from sklearn import  metrics
 import matplotlib.pyplot as plt 
 
 def getvector(idx,mouse,goal,label):
-    def goalend(mouse,goal):
-        n=len(mouse[0])
-        ex=mouse[0][n-1]
-        ey=mouse[1][n-1]
-        gx=goal[0]
-        gy=goal[1]
-        tmp=(gx-ex)**2+(gy-ey)**2
-        tmp=tmp**0.5
-        return [(gx-ex)/tmp,(gy-ey)/tmp]
-    def startend(mouse):
-        n=len(mouse[0])
-        ex=mouse[0][n-1]
-        ey=mouse[1][n-1]
-        et=mouse[2][n-1]
-        bx=mouse[0][0]
-        by=mouse[1][0]
-        bt=mouse[2][0]
-        if n>1:
-            tmp=(bx-ex)**2+(by-ey)**2+(by-ey)**2
-        else:
-            tmp=(bx)**2+(by)**2+(by)**2
-            bx=by=bt=0.0
-        tmp=tmp**0.5
-        if tmp<1e-3:
-            tmp=1.0
-        return [(ex-bx)/tmp,(ey-by)/tmp,(et-bt)/tmp]
     def getyn(mouse):
         yn=len(mouse[1])
         y=mouse[1]
         if yn==1:
-            return [0]
+            return [0.0,0.0]
+            # return [0.0]
         for i in range(yn)[-1:0:-1]:
             y[i]=y[i]-y[i-1]
         flag=1
@@ -50,270 +25,130 @@ def getvector(idx,mouse,goal,label):
             if state*y[i]<0:
                 ychange+=1
             state=y[i]
-        return [float(ychange)]
-    def gett(mouse):
-        tn=len(mouse[2])
-        t=mouse[2]
-        if tn==1:
-            return [0.0]
-        return [(t[tn-1]-t[0])/1000]
-    def getxspeed(mouse):
-        tn=len(mouse[2])
-        t=mouse[2]
+        tmp=y/y.sum() 
+        # return [float(tmp.std()*3.0)]
+        return [float(ychange)/5.0,float(tmp.std()*3.0)]
+    def getlastt(mouse):
+        xn=len(mouse[0])
         x=mouse[0]
-        if tn==1:
-            return [0]
-        for i in range(tn)[-1:0:-1]:
-            x[i]=x[i]-x[i-1]
-            t[i]=t[i]-t[i-1]
-            if t[i]>0:
-                x[i]=x[i]/t[i]
-            else:
-                x[i]=0.0
-        x=np.array(x)[1:]
-        return [x.mean()/10]
+        y=mouse[1]
+        t=mouse[2]
+        idxget=0
+        idxgetl=xn-1
+        vs_all=0.0
+        ve_all=0.0
+        for i in range(4):
+            if idxget<xn-1:
+                idxget+=1
+            if idxgetl>0:
+                idxgetl-=1
+
+            vs=(float(x[idxget])-float(x[idxget-1]))/1000
+            ts=(float(t[idxget])-float(t[idxget-1]))/1000
+            ts= ts if ts>1e-5 else 1.0
+            vs/=ts
+            idxb=idxgetl-1 if idxgetl>0 else 0
+            ve=(float(x[idxgetl])-float(x[idxb]))/1000
+            te=(float(t[idxgetl])-float(t[idxb]))/1000
+            te= te if te>1e-5 else 1.0
+            ve/=te
+
+            vs_all+=vs
+            ve_all+=ve
+
+
+        percent=float(t[idxget])/float(t[xn-1])
+        percent=percent*10.0 if percent*10.0<1.0 else 1.0
+        
+        percentl=float(t[idxgetl])/float(t[xn-1])
+        percentl=percentl*1.0 if percentl*1.0<1.0 else 1.0
+
+        return [percent/2,percentl/1.5,vs_all/5.0,ve_all/3]
+    def getlastangle(mouse):
+        xn=len(mouse[0])
+        x=mouse[0]
+        y=mouse[1]
+        t=mouse[2]
+        # idxget=0
+        idxgetl=xn-1
+        for i in range(5):
+            # if idxget<xn-1:
+            #     idxget+=1
+            if idxgetl>0:
+                idxgetl-=1
+
+        cx=(float(x[xn-1])-float(x[idxgetl]))/1000
+        cy=(float(y[xn-1])-float(y[idxgetl]))/1700/3
+        ct=(float(t[xn-1])-float(t[idxgetl]))/1000
+        if ct<1e-3:
+            ct=1.0
+        anglex=3.0*cx/ct
+        angley=10.0*cy/ct
+        # percentl=float(t[idxgetl])/float(t[xn-1])
+        # percentl=percentl*1.0 if percentl*1.0<1.0 else 1.0
+        return [anglex,angley]
+    def getlastgoal(mouse,goal):
+        xn=len(mouse[0])
+        x=mouse[0]
+        y=mouse[1]
+        tmpx=float(goal[0])-float(x[len(x)-1])
+        tmpy=float(goal[1])-float(y[len(y)-1])
+        return [tmpx/300,tmpy/3000]
 
     tmp=[]
-    tmp.extend([mouse[0][0]/1000,mouse[1][0]/1000])
-    # tmp.extend(goalend(mouse,goal))
-    # tmp.extend(startend(mouse))
+    # mouse[1][0]/1700/3,
+    tmp.extend([mouse[0][0]/1000,mouse[2][0]/1000])
+
     tmp.extend(getyn(mouse))
-    # tmp.extend(gett(mouse))
-    # tmp.extend(getxspeed(mouse))
-    # np.array([xarr[0],yarr[0]]).reshape([1,2])
+    # tmp.extend(getlastt(mouse))
+    tmp.extend(getlastangle(mouse))
+    tmp.extend(getlastgoal(mouse,goal))
+
+
     return np.array(tmp).reshape([1,len(tmp)])
-   
+
 def assemble():
     ds=dataset.DataSet()
     ds.getTrainData()
-    dw=datadraw.DataDraw('2d')
+    # dw=datadraw.DataDraw('2d')
     mouses=ds.train["mouses"]
     goals=ds.train["goals"]
     labels=ds.train["labels"]
     n=ds.train["size"]
     vector=[]
+
     # print mouses[492],goals[492]
     for i in range(n):
-        vector.append(getvector(1,mouses[i],goals[i],1))
+        vector.append(getvector(1,mouses[i],goals[i],1)[0])
     vector=np.array(vector)
-    # print vector.shape
 
     dt=datadeal.DataTrain()
-    # clf = MLPClassifier(alpha=1e-3, hidden_layer_sizes=(40), random_state=1)
-    clf = SVC()
-    dt.trainTest(clf,vector,labels)
-
-def getAssembleResult():
-    ds=dataset.DataSet()
-    # ds.test_file_path=ds.train_file_path
-    ds.getTrainData()
-    dt=datadeal.DataTrain()
-    clf=SVC(C=0.55)
-    # clf = MLPClassifier(alpha=1e-5, hidden_layer_sizes=(10), random_state=1)
-    y=ds.train["labels"]
-    mouses=ds.train["mouses"]
-    goals=ds.train["goals"]
-    labels=ds.train["labels"]
-    n=ds.train["size"]
-    X=[]
-    for i in range(n):
-        X.append(getvector(1,mouses[i],goals[i],1)[0])
-        # print X
-        # exit()
-    X=np.array(X)
-    # print X.shape
-    # dt.trainTest(clf,X,y)
-    dt.train(clf,X,y)
-
-    # dt.testResultAll(ds,getvector,savepath='./data/0607tmp.txt')
-
-    dt.testResultAll(ds,getvector,savepath='./data/assemble.txt')
-
-def dataanalyst():
-    with open('./data/0607tmp.txt','r') as f:
-        r=f.read()
-    ra=r.split('\n')
-    # print ra
-    wrong=[]
-    notget=[]
-    tmparr=[0]*3000
-    strwrong=''
-    strnotget=''
-    for v in ra:
-        if v=='':
-            continue
-        if int(v)<2600:
-            wrong.append(v)
-            strwrong+=v+"\n"
-        tmparr[int(v)-1]=1
-    for i in range(2600,3000):
-        if tmparr[i]==0:
-            notget.append(i+1)
-            strnotget+=str(i+1)+"\n"
-    # print wrong,len(wrong)
-    # print notget,len(notget)
-
-    with open('./data/wrong.txt','w') as f:
-        f.write(strwrong)
-    with open('./data/notget.txt','w') as f:
-        f.write(strnotget)
-
-def wronganalyst():
-    ds=dataset.DataSet()
-    ds.getTrainData()
-    dt=datadeal.DataTrain()
+    clf = MLPClassifier(alpha=1e-6,activation='logistic', hidden_layer_sizes=(20,20),\
+     random_state=0,solver='lbfgs')
+    # clf = SVC(C=1.35,kernel='poly',degree=4,gamma=1,coef0=1.6)
     
-    y=ds.train["labels"]
-    mouses=ds.train["mouses"]
-    goals=ds.train["goals"]
-    labels=ds.train["labels"]
-
-    with open('./data/wrong.txt','r') as f:
-        s=f.read()
-    wrongs=s.split()
-    idxw=np.array(wrongs,dtype='int')-1
-    # print len(idxw)
-    mousew=[mouses[i] for i in idxw]
-    # print len(mousew)
-    # mousew=mouses[]
-    # print idxw
-    dw=datadraw.DataDraw('3d')
-    colors=dw.getColorsValue()
-    count=0
-    dw.draw3dline(mouses[2601],c=colors[count%10])
-    for i in range(len(mousew)):
-        count+=1
-        if count>4:
-            break
-        dw.draw3dline(mousew[i],c=colors[count%10])
-        # goal=goals[i]
-        # dw.draw3dgoal([goal[0],goal[1]+2000,0],c=colors[count%10])
-    plt.show()
-
-def notgetanalyst():
-    ds=dataset.DataSet()
-    ds.getTrainData()
-    dt=datadeal.DataTrain()
-    
-    y=ds.train["labels"]
-    mouses=ds.train["mouses"]
-    goals=ds.train["goals"]
-    labels=ds.train["labels"]
-
-    with open('./data/notget.txt','r') as f:
-        s=f.read()
-    wrongs=s.split()
-    idxw=np.array(wrongs,dtype='int')-1
-    # print len(idxw)
-    mousew=[mouses[i] for i in idxw]
-    # print len(mousew)
-    # mousew=mouses[]
-    # print idxw
-    dw=datadraw.DataDraw('3d')
-    colors=dw.getColorsValue()
-    count=0
-    
-
-    dw.draw3dline(mouses[0],c=colors[count%10])
-    for i in range(len(mousew)):
-        count+=1
-        if count>10:
-            break
-        dw.draw3dline(mousew[i],c=colors[count%10])
-        # goal=goals[i]
-        # dw.draw3dgoal([goal[0],goal[1]+2000,0],c=colors[count%10])
-    plt.show()
-
-def analystTestData():
-    ds=dataset.DataSet()
-    ds.getTrainData()
-    dt=datadeal.DataTrain()
-    idxs=[]
-    mouses=[]
-    goals=[]
-
-    for i in range(1000):
-        idx,mouse,goal,_=ds.readTestFile()
-        if mouse[1][0]<0:
-            mouse[1][0]=2100
-        if mouse[0][0]>1000:
-            mouse[0][0]-=400
-
-        mouses.append([mouse[0][0],mouse[1][0]])
-        goals.append(goal)
-
-    with open('./data/BDC1236_20170607.txt','r') as f:
-        while True:
-            idx=f.readline()
-            idx=int(idx)
-            if idx>1000:
-                break
-            idxs.append(idx)
-    idxs=np.array(idxs)
-    idxs=idxs-1
-    # print sum(idxs)
-    mouses=np.array(mouses)
-    dw=datadraw.DataDraw('2d')
-    dw.drawbatchgoal(mouses,c='b')
-    dw.drawbatchgoal(mouses[idxs],c='r')
-    plt.show()
-
-
-    # dt.testResultAll(ds,getvector,savepath='./data/assemble.txt')
-
-def analystTestData2():
-    ds=dataset.DataSet()
-    ds.getTrainData()
-    dt=datadeal.DataTrain()
-    idxs=[]
-    mouses=[]
-    goals=[]
-
-    for i in range(1000):
-        idx,mouse,goal,_=ds.readTestFile()
-        mouses.append(mouse)
-        goals.append(goal)
-    # print goals
-    with open('./data/BDC1236_20170607.txt','r') as f:
-        while True:
-            idx=f.readline()
-            idx=int(idx)
-            if idx>1000:
-                break
-            idxs.append(idx)
-    idxs=np.array(idxs)
-    print idxs
-    # print mouses[0]
-    dw=datadraw.DataDraw('3d')
-    colors=dw.getColorsValue()
-    start=10
-    # for i in range(10):
-    #     dw.draw3dline(mouses[start+i],c=colors[i%10])
-    # plt.savefig('./a.png')
-
-    # plt.show()
-    # print idxs
-    # idxs=idxs-1
-    # print sum(idxs)
-    # mouses=np.array(mouses)
-    # 
-    # dw.drawline(mouses[0])
-    # # dw.drawbatchgoal(mouses,c='b')
-    # # dw.drawbatchgoal(mouses[idxs],c='r')
-    # plt.show()
-
-
-    # dt.testResultAll(ds,getvector,savepath='./data/assemble.txt')
+    print vector[0]
+    print vector[10]
+    print vector[1000]
+    print vector[2700]
+    print vector[2800]
+    print vector[2900]
+    # exit()
+    # test=False
+    test=False
+    if test==True:
+        dt.trainTest(clf,vector,labels)
+    else:
+        dt.train(clf,vector,labels)
+        dt.testResultAll(ds,getvector,savepath='./data/0624tmp.txt')
 
 
 if __name__=="__main__":
-    # assemble()
-    # getAssembleResult()
-    # 84.74 16573  =>15205  1000 176
-    # print datadeal.calcScoreRerve(0.8474,16573.0)
-    # dataanalyst()
-    # wronganalyst()
-    notgetanalyst()
-    # analystTestData()
-    # analystTestData2()
+    # 85.67 16059 15108
+    # print datadeal.calcScoreRerve(0.9306,22400.0)
+    # analystnoget()
+    tmp=assemble()
+    # tmp=np.loadtxt('./data/tmp0619.txt')
+    # tmp=np.array(tmp,dtype='int')
+  
     pass
